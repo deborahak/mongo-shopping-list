1cb906b85816d117083c1364dbdff32bb3e11670
deborahak <deborahkain@mac.com>
2017-03-03 08:38:53 -0800

    heroku

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+exports.DATABASE_URL = process.env.DATABASE_URL ||  global.DATABASE_URL || 'mongodb://localhost/shopping-list-dev';
 exports.PORT = process.env.PORT || 8080;
